906e0705d5982df1382cb725e25e783703a5a692
Sarin227 <cmsacco@nic.edu>
2024-10-23 17:29:42 +0000

    more

--- conflict.cpp
+++ conflict.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main()
 {
+    int num1 = 0;
     cout<< "Hello World\n";
     cout<< "Wow\n";
     // More code
+    cin>> num1;
+   
+    cout<< num1;
     return 0;
 }
